857076c53dc51111992f36ab3165b23f2f2c74c2
Gabriel Lima <gabriel.lima137170@gmail.com>
2022-08-04 23:02:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/androidsql/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.androidsql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mysql.cj.protocol.Resultset;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt_1;
+    TextView tv_1, tv_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt_1 = findViewById(R.id.bt_1);
+        tv_1 = findViewById(R.id.tv_1);
+        tv_2 = findViewById(R.id.tv_2);
+
+        bt_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Task().execute();
+            }
+        });
+    }
+
+    class Task extends AsyncTask<Void, Void, Void>{
+        String records = "", error="";
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+
+                Class.forName("org.postgresql.Driver");
+                //Connection connection = DriverManager.getConnection("jdbc:postgresql:postgres://eanhrepj:aXOoO0LEXJPkQoexanTF-ZbUCxo1tMt_@kesavan.db.elephantsql.com/eanhrepj","eanhrepj","aXOoO0LEXJPkQoexanTF-ZbUCxo1tMt_");
+
+                //Connection connection = DriverManager.getConnection("jdbc:postgresql:postgres://kesavan.db.elephantsql.com/eanhrepj","eanhrepj","aXOoO0LEXJPkQoexanTF-ZbUCxo1tMt_");
+
+                Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","root");
+
+                //Connection connection = DriverManager.getConnection("jdbc:postgresql://tesaoeanhrepj:aXOoO0LEXJPkQoexanTF-ZbUCxo1tMt_@kesavan.db.elephantsql.com/eanhrepj","eanhrepj","aXOoO0LEXJPkQoexanTF-ZbUCxo1tMt_");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT username FROM accounts");
+
+                while(resultSet.next()) {
+                    records += resultSet.getString(1);
+                }
+
+            } catch(Exception e) {
+                error = e.toString();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            tv_1.setText(records);
+            if (error != "") {
+                tv_2.setText(error);
+            }
+
+            super.onPostExecute(unused);
+        }
+    }
+
+}
